1e1ce6a9920acfb4e1302a23b0da0675315dba5f
Madické Thiane <madicke.thiane@gmail.com>
2022-03-15 09:52:50 +0100

    Exo 1

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-web";
+import DragonB from "./assets/dragon-ball.json";
+import Card from "./components/Card";
 
 export default function App() {
+  const [show, setShow] = useState(false);
+
+  const renderData = ({ item }) => (
+    <Card item={item} show={show} display={() => setShow(!show)} />
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Données Dragon Ball</Text>
+      <FlatList
+        data={DragonB.characters}
+        renderItem={renderData}
+        keyExtractor={(datas) => datas.id}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/Card.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+export default function Card({ item, show, display }) {
+  return (
+    <View style={styles.card}>
+      <Text>Nom du personnage : {item.name}</Text>
+      <Text>Capacités du personnage : {item.abilities}</Text>
+      {show && <Text style={{ color: "blue" }}>{item.bio}</Text>}
+      <Pressable onPress={() => display()}>
+        <Text style={{ color: "red" }}>
+          {!show ? "Voir la bio" : "Cacher la bio"}
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderWidth: 4,
+    borderRadius: 10,
+    padding: 5,
+    margin: 10,
+  },
+});
